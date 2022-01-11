300b2f6679d584949cb6f8a8874e3556b8eafca7
Pratik Shah <pratik@infracloud.io>
2022-01-11 23:54:38 +0530

    Modified vbmc image to download certificates from artifactory

---
+++ resources/vbmc/download-certificates.sh
@@ -0,0 +1,5 @@
+mkdir -p /etc/pf9/certs/libvirt/
+cd /etc/pf9/certs/libvirt/
+curl https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/ca/cert.pem -o cacert.pem
+curl https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/hostagent/cert.pem -o clientcert.pem
+curl https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/hostagent/key.pem -o clientkey.pem 
